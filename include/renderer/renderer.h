5a51f320f9819ec893e71fa0c18e624c27865674
IliyaBadri <iliya0badri@gmail.com>
2025-03-16 14:48:36 +0330

    Fixed renderer error messages

--- include/renderer/renderer.h
+++ include/renderer/renderer.h
@@ -6,12 +6,12 @@
 
 void renderer_render_object(SDL_Renderer* renderer, Object* object){
     if(renderer == NULL){
-        fprintf(stderr, "Fatal error: NULL pointer renderer passed to include/Renderer/ObjectRenderer.h -> renderer_render_object().\n");
+        fprintf(stderr, "Fatal error: NULL pointer renderer passed to iinclude/renderer/renderer.h-> renderer_render_object().\n");
         exit(EXIT_FAILURE);
         return;
     }
     if(object == NULL){
-        fprintf(stderr, "Fatal error: NULL pointer object passed to include/Renderer/ObjectRenderer.h -> renderer_render_object().\n");
+        fprintf(stderr, "Fatal error: NULL pointer object passed to include/renderer/renderer.h -> renderer_render_object().\n");
         exit(EXIT_FAILURE);
         return;
     }
